b9cf50d7e8826c2d4722185ac2bda678836138eb
aimeric-sr <aimeric.sorin@epsi.fr>
2022-01-11 17:55:03 +0100

    new branch

--- src/api/v1/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routerUser = require('./users');
-const routerAuth = require('./auths');
-const router = express.Router();
-
-router.use('/users', routerUser);
-router.use('/auth', routerAuth);
-
-module.exports = router;
